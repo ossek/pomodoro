b647a3fe6d39b4df1cd392131043edb4101db9d2
ossek <ossek.20.sherlocken@spamgourmet.com>
2013-12-06 00:06:47 -0600

    initial commit

---
+++ pomodoro.py
@@ -0,0 +1,66 @@
+""" The flask service for the pomordoro application"""
+import os
+from os import path
+from flask import Flask
+#from flask import render_template
+from flask import make_response
+import flask.helpers as helpers
+
+class PomodoroApplicationService(object):
+    """ The class containing the service """
+    app = Flask(__name__)
+    # static content will be served by the proxy.  to try to do that
+    # with flask, see
+    # http://werkzeug.pocoo.org/docs/middlewares/
+    # http://flask.pocoo.org/docs/quickstart/#hooking-in-wsgi-middlewares
+    
+    @classmethod
+    def get_flask_app(cls):
+        """ get hold of the flask app"""
+        return cls.app
+
+    @staticmethod
+    @app.route('/')
+    def site_root():
+        """site's root method"""
+        response = make_response(open('./app/index.html').read())
+        return response
+    
+    @classmethod
+    @app.route('/favicon.ico')
+    def favicon(cls):
+        """little icon for fav"""
+        favicon_path = os.path.join('app', 'img', 'favicon.ico')
+        helpers.send_file(favicon_path)
+    	
+    @classmethod
+    @app.route('/<path:urlpath>/')
+    def other_paths(cls, urlpath):
+        """ for all other paths"""
+        if urlpath == "favicon.ico":
+            return ""
+        file_system_path = path.join('./', urlpath)
+        if os.path.isdir(file_system_path):
+            return cls.render_directory(file_system_path)
+        else:
+            opened_file = open(file_system_path)
+            readed = opened_file.read()
+            stringresponse = make_response(readed)
+            return stringresponse
+    
+    @classmethod
+    def render_directory(cls, dirpath):
+        """show directory contents (just list of strings)"""
+        print("------dirpath---------" + dirpath)
+        list_contents = os.listdir(dirpath)
+        print("------list contents----" + str(list_contents))
+        response_string = ""
+        for entry in list_contents:
+            response_string += entry + '\n'
+        print("---the responseString------" + str(response_string))
+        response = make_response(''.join(response_string))
+        return response
+
+if __name__  == '__main__':
+    PomodoroApplicationService.get_flask_app().debug = True
+    PomodoroApplicationService.get_flask_app().run()
