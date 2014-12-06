f59954db344db91797aabaa400d756ace8adf91b
ossek <github.20.sherlocken@spamgourmet.com>
2014-12-06 17:42:55 -0600

    refactor to services

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,16 +1,15 @@
-define(['angular','services','timey','parameterCheck','timbre'],function(angular,services,timey,parameterCheck,timbre){
+define(['angular','services','timey','parameterCheck'],function(angular,services,timey,parameterCheck,timbre){
   'use strict';
   var pomodoroControllers = angular.module('pomodoro.controllers', ['pomodoro.services']);
-  
-  pomodoroControllers.controller('pomodoroCtrl', ['$scope','$routeParams','$interval',
-    function($scope,$routeParams,$interval){
+  pomodoroControllers.controller('pomodoroCtrl', [
+      '$scope',
+      '$routeParams',
+      '$interval',
+      'timeConvertService',
+      'soundService',
+    function($scope,$routeParams,$interval,timeConvertService,soundService){
       var timer = Object.create(timey);
       var TWENTY_FIVE_IN_MILLIS_EPOCH = 1500000;
-      var pluckA = timbre('pluck',{freq:440,mul:1.0});
-      var pluckC = timbre('pluck',{freq:261.626,mul:1.0});
-      var pluckE = timbre('pluck',{freq:391.995,mul:1.0});
-      var pluckG = timbre('pluck',{freq:329.628,mul:1.0});
-
 
       $scope.timerDisplay = {
               pauseAvailable : false,
@@ -98,22 +97,6 @@ define(['angular','services','timey','parameterCheck','timbre'],function(angular
 	      }
       };
 
-      function getMillisFromInput(inputHours,inputMins,inputSecs){
-	      var hoursMillis = 0;
-	      var minuteMillis = 0;
-	      var secMillis = 0;
-	      if(!(inputHours === null || inputHours === undefined)){
-		      hoursMillis = inputHours*60*60*1000;
-	      }
-	      if(!(inputMins === null || inputMins === undefined)){
-		      minuteMillis = inputMins*60*1000;
-	      }
-	      if(!(inputSecs === null || inputSecs === undefined)){
-		      secMillis = inputSecs*1000;
-	      }
-	      return hoursMillis + minuteMillis + secMillis;
-      }
-
       function setEditorInactive(){
 	      $scope.timerDisplay.editorActive = false;
       }
@@ -152,9 +135,7 @@ define(['angular','services','timey','parameterCheck','timbre'],function(angular
 	                time : timer.getHourMinuteSecondString(timerFinishedEventObj.completedCountdownMillis),
 	                at : timer.getLocalTimeString(timerFinishedEventObj.finishedAt),
 	        });
-                pluckC.bang().play();
-                pluckE.bang().play();
-                pluckG.bang().play();
+                soundService.playTimerFinishedSound();
 	      }
 	      setPauseUnavailable();
 	      setResumeUnavailable();
@@ -169,7 +150,7 @@ define(['angular','services','timey','parameterCheck','timbre'],function(angular
       },20,0,true);
 
       function startTimer(){
-         var inputMillis = getMillisFromInput($scope.timerDisplay.inputHours,
+         var inputMillis = timeConvertService.getMillisFromInput($scope.timerDisplay.inputHours,
 			 $scope.timerDisplay.inputMinutes,
 			 $scope.timerDisplay.inputSeconds);
 	 if(inputMillis === null){
--- app/js/services.js
+++ app/js/services.js
@@ -1,120 +1,45 @@
-define(['angular','parameterCheck'],function(angular,parameterCheck){
+define(['angular','timbre'],function(angular,timbre){
   'use strict';
   
-  /* Services */
-  //http://docs.angularjs.org/api/angular.module
-  
-  // Demonstrate how to register services
-  // In this case it is a simple value service.
-  //angular.module('pomodoro.services', []).
-  //  value('version', '0.1');
-  
-  //When passed two or more arguments, a new module is created. 
-  //If passed only one argument, an existing module 
-  //(the name passed as the first argument to module) is retrieved.
   var services_module = angular.module('pomodoro.services',[]);
-  //the .value method is a sugar or convenience
-  //http://docs.angularjs.org/guide/module#module-loading-&-dependencies_configuration-blocks
-  //services_module.value('countdown_service',
   
-  var isInteger = parameterCheck.isInteger;
-  
-  services_module.factory('countdownService',['$interval',function(interval){
-      var _startDate;
-      var _countdownFromMillis;
-      var _timeoutId;
-      //start these each at 0 in the case where we want to 
-      //run a timer with 0 value
-      var _elapsedMillis = 0;
-      var _remain = 0;
-      var updatePeriodMillis = 1000;
-      
-      var startTimer = function(countdownFromMillis){
-  	if(typeof(countdownFromMillis) !== "number" || !isInteger(countdownFromMillis)){
-            throw "countdownFromMillis must be an integer";
-  	}
-  	if(countdownFromMillis < 0){
-            throw "cannot start timer with a value < 0";
-  	}
-  	//TODO either clear all private data or make it
-  	//so each service dependency is uniquely instantiated
-  	_countdownFromMillis = countdownFromMillis;
-  	_startDate = Date.now();
-  	//start ticking
-  	_timeoutId = interval(function(){
-            updateTimeRemaining();
-            }, updatePeriodMillis);
-      };
-  
-      var updateTimeRemaining = function() {
-        _elapsedMillis = Date.now() - _startDate;
-        if (_elapsedMillis >= _countdownFromMillis){
-	    //the elapsed millis should not show elapsing past the interval, 
-	    //since we are approximating stopping there
-	        _elapsedMillis = _countdownFromMillis;
-                cancelCountdown(_timeoutId);
-                _remain = 0;
-        }
-        else{
-          //if this calculation is delayed, could still be negative
-          _remain = _countdownFromMillis - _elapsedMillis;
-        }
-      };
-  
-      var getTimeRemainingMillis = function(){
-  	return _remain;
-      };
-  
-      var getElapsedMillis = function(){
-  	return _elapsedMillis;
-      };
-  
-      var getCountdownHourMinuteSecString = function(){
-  	var countdownValue = getHourMinuteSecondRemainString(getTimeRemainingMillis);
-  	return countdownValue;
-      };
-  
-      var cancelCountdown = function(){
-        interval.cancel(_timeoutId);
-      };
-  
-      var getHourMinuteSecondRemainString = function() {
-  	return getHourMinuteSecondString(_remain);
-      };
-  
-      var reset = function(){
-  	cancelCountdown();
-          var _startDate = 0;
-          var _countdownFromMillis = 0;
-          var _timeoutId = 0;
-          //start these each at 0 in the case where we want to 
-          //run a timer with 0 value
-          var _elapsedMillis = 0;
-          var _remain = 0;
-      };
-  
-      var getHourMinuteSecondString = function(millis) {
-  	//this constructor is milliseconds with respect to UTC epoch start
-  	// so showing its value with non-UTC methods will adjust for 
-  	// browser's timezone (i think that's what's happening?)
-  	var dateFromMillis = new Date(millis); 
-  	var utcString = dateFromMillis.toUTCString();
-          var outputString = utcString.slice(-12,utcString.length - 4);
-  	return outputString;
-      };
-  
-  
+  services_module.factory('timeConvertService',[function(){
+      function getMillisFromInput(inputHours,inputMins,inputSecs){
+	      var hoursMillis = 0;
+	      var minuteMillis = 0;
+	      var secMillis = 0;
+	      if(!(inputHours === null || inputHours === undefined)){
+		      hoursMillis = inputHours*60*60*1000;
+	      }
+	      if(!(inputMins === null || inputMins === undefined)){
+		      minuteMillis = inputMins*60*1000;
+	      }
+	      if(!(inputSecs === null || inputSecs === undefined)){
+		      secMillis = inputSecs*1000;
+	      }
+	      return hoursMillis + minuteMillis + secMillis;
+      }
       return {
-  	startTimer : startTimer,
-  	getTimeRemainingMillis: getTimeRemainingMillis,
-  	getHourMinuteSecondRemainString: getCountdownHourMinuteSecString,
-        getElapsedMillis: getElapsedMillis,
-  	cancelCountdown : cancelCountdown,
-	timeRemaining : _remain,
-  	reset : reset
+          getMillisFromInput : getMillisFromInput
       };
     }
   ]);
+  
+  services_module.factory('soundService',[function(){
+      var pluckC = timbre('pluck',{freq:261.626,mul:1.0});
+      var pluckE = timbre('pluck',{freq:391.995,mul:1.0});
+      var pluckG = timbre('pluck',{freq:329.628,mul:1.0});
+      function playTimerFinishedSound(){
+                pluckC.bang().play();
+                pluckE.bang().play();
+                pluckG.bang().play();
+      }
+      return {
+          playTimerFinishedSound : playTimerFinishedSound
+      };
+    }
+  ]);
+
 
 });
 
