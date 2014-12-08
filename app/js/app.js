58e01d3eb275d6f7e4f87b329e10a5d583a311a9
ossek <github.20.sherlocken@spamgourmet.com>
2014-12-07 21:57:44 -0600

    remove comment

--- app/js/app.js
+++ app/js/app.js
@@ -20,8 +20,6 @@ var pomodoroModule = angular.module('pomodoro', [
 pomodoroModule.config(['$routeProvider', function($routeProvider) {
     console.log('making routes');
   $routeProvider.when('/pomodoro', {templateUrl: 'partials/pomodoro.html', controller: 'pomodoroCtrl'});
-  //$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  //TODO query url to set pomodoro time
   $routeProvider.otherwise({redirectTo: '/pomodoro'});
 }]);
 
